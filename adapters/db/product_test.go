d5e0e202d99f6460c0cca7962bc33f4c0dd21061
Everton Zoelner <everton.zoelner@gmail.com>
2024-07-15 20:41:07 -0300

    test: Criando testes do método Get

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -2,12 +2,15 @@ package db_test
 
 import (
 	"database/sql"
+	"github.com/stretchr/testify/require"
+	"github.com/zoelner/hexagonal-architecture/adapters/db"
 	"log"
+	"testing"
 )
 
 var Db *sql.DB
 
-func setup() {
+func setUp() {
 	Db, _ = sql.Open("sqlite3", ":memory:")
 	createTable(Db)
 	createProduct(Db)
@@ -34,4 +37,15 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
 }
